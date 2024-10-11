9a5f03e555b2d75f8eeb9fa4670469476f1f0bac
Jonathan Vazquez <jonavazqtc@gmail.com>
2024-10-10 22:03:56 -0600

    Fin de HOF

--- Intermediate/04_higher_order_functions.py
+++ Intermediate/04_higher_order_functions.py
@@ -1,1 +1,16 @@
 ### Higher Order Functions ###
+
+def sum_one(value):
+    return value + 1
+
+def sum_five(value):
+    return value + 5
+"""
+def sum_two_values_and_add_one(first_value, second_value):
+    return sum_one(first_value + second_value)
+"""
+def sum_two_values_and_add_value(first_value, second_value, f_sum):
+    return f_sum(first_value + second_value)
+
+print(sum_two_values_and_add_value(5, 2, sum_one))
+print(sum_two_values_and_add_value(5, 2, sum_five))
