ec344f9e6b9f7e76df58e4b8e41514df6eb79aac
Jonathan Vazquez <jonavazqtc@gmail.com>
2024-10-10 22:14:51 -0600

    Ejercicio de Closures

--- Intermediate/04_higher_order_functions.py
+++ Intermediate/04_higher_order_functions.py
@@ -14,3 +14,17 @@ def sum_two_values_and_add_value(first_value, second_value, f_sum):
 
 print(sum_two_values_and_add_value(5, 2, sum_one))
 print(sum_two_values_and_add_value(5, 2, sum_five))
+
+### Closures ###
+# Los closures retornan una funcion
+
+def sum_ten(original_vale):
+    def add(value):
+        return value + 10 + original_vale
+    return add
+
+### sum_ten retorna la función add
+add_closure = sum_ten(1)    
+print(add_closure(5))
+
+sum_ten(5)(1)
