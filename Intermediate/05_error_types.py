4726245088cb9f6d0341085a5f36a7399783a105
Jonathan Vazquez <jonavazqtc@gmail.com>
2024-10-10 23:10:03 -0600

    Index error

--- Intermediate/05_error_types.py
+++ Intermediate/05_error_types.py
@@ -1,7 +1,16 @@
 ### Error Types ###
 
 # SyntaxError
-#print "Hola comunidad!" # Error
+#print "Hola comunidad!" # Remover comentario para el error
 print ("!Hola comunidad!")
 
 # NameError
+language = "Spanish" # Remover comentario para el error
+print(language)
+
+# IndexError
+my_list = ["Python", "Swift", "Kotlin", "Dart", "Javascript"]
+print(my_list[0])
+print(my_list[4])
+print(my_list[-1])
+#print(my_list[5]) # Remover comentario para el error
