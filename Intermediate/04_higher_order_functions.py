f774152d547fba8240b62e01b13b57cbc4a16901
Jonathan Vazquez <jonavazqtc@gmail.com>
2024-10-10 22:25:03 -0600

    Mi ejercicion de closures

--- Intermediate/04_higher_order_functions.py
+++ Intermediate/04_higher_order_functions.py
@@ -27,4 +27,47 @@ def sum_ten(original_vale):
 add_closure = sum_ten(1)    
 print(add_closure(5))
 
+# Es como una ejecucion de una lambda, en donde el primer parametro es para sum_ten y el segundo
+# es el parametro de la función dentro de sum_ten
 sum_ten(5)(1)
+
+# Mi ejercicio de closures
+SUMAR = 1
+RESTAR = 2
+MULTIPLICAR = 3
+DIVIDIR = 4
+
+def math_operations(operator):
+    if SUMAR == operator:
+        def sumNumbers(first_value, second_value):
+            return first_value + second_value
+        return sumNumbers
+    elif RESTAR == operator:
+        def resNumbers(first_value, second_value):
+            return first_value - second_value
+        return resNumbers
+    elif MULTIPLICAR == operator:
+        def multNumbers(first_value, second_value):
+            return first_value * second_value
+        return multNumbers
+    else:
+        def divNumbers(first_value, second_value):
+            return first_value / second_value
+        return divNumbers
+
+def f_(operator):
+    # Si solo se mandar llamar math_operations(operator) sin return antes, 
+    # La función f_ retorna un error de que la función no es llamable
+    return math_operations(operator)
+
+"""    
+print(math_operations(SUMAR)(2,2))
+print(math_operations(RESTAR)(2,2))
+print(math_operations(MULTIPLICAR)(2,2))
+print(math_operations(DIVIDIR)(2,2))
+"""    
+
+print(f_(SUMAR)(2,2))
+print(f_(RESTAR)(2,2))
+print(f_(MULTIPLICAR)(2,2))
+print(f_(DIVIDIR)(2,2))
